1e217fd6b4fcc6f5408c11518a9dae8acc322a55
chaniru05 <chanirusenarathne05@gmail.com>
2025-09-30 22:55:26 +0530

    nav.jsx default done

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,6 @@ function App() {
   return (
     <div>
       <Nav />   {/* Render Navbar */}
-      <main>
-        <h1>Welcome to My Vite + React App 🎉</h1>
-        <p>This is where your content goes.</p>
-      </main>
     </div>
   );
 }
