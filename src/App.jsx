e0bfb40eb9323aae6a8402edbbb54e221d7c8457
chaniru05 <chanirusenarathne05@gmail.com>
2025-10-01 00:52:33 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from "./components/nav";
+import Foot from './components/footer';
 
 function App() {
   return (
     <div>
       <Nav />   {/* Render Navbar */}
+      <Foot/>   {/*Render Footer*/}
     </div>
   );
 }
