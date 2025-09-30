374654e36065b1311f14283cbf0ddc96475eb23d
chaniru05 <chanirusenarathne05@gmail.com>
2025-09-30 20:59:17 +0530

    nv.jsx update1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import { Nav } from './components/nav'
+import Nav from "./components/nav";
 
+function App() {
+  return (
+    <div>
+      <Nav />   {/* Render Navbar */}
+      <main>
+        <h1>Welcome to My Vite + React App 🎉</h1>
+        <p>This is where your content goes.</p>
+      </main>
+    </div>
+  );
+}
 
-
-
-export default App
+export default App;
