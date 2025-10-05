4027e2749ab954b5a28b5708ca1ceaea0ddb14d6
minurisiyara <minurisiyara003@gmail.com>
2025-10-05 21:01:09 +0530

    Update AboutUs.jsx
    aboutUS changed

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,14 +1,14 @@
 const links = [
-  { name: 'Open roles', href: '#' },
-  { name: 'Internship program', href: '#' },
-  { name: 'Our values', href: '#' },
-  { name: 'Meet our leadership', href: '#' },
+  { name: 'Explore Features', href: '#' },
+  { name: 'Find Vendors', href: '#' },
+  { name: 'Testimonials', href: '#' },
+  { name: 'Contact Us', href: '#' },
 ]
 const stats = [
-  { name: 'Offices worldwide', value: '12' },
-  { name: 'Full-time colleagues', value: '300+' },
-  { name: 'Hours per day', value: '40' },
-  { name: 'Paid time off', value: 'Unlimited' },
+  { name: 'Happy Couples Served', value: '100+' },
+  { name: 'Planning Checklists', value: '300+' },
+  { name: 'Support Available', value: '24/7' },
+  { name: 'Planning Guaranteed', value: 'Stress-Free' },
 ]
 
 export default function Example() {
@@ -24,7 +24,7 @@ export default function Example() {
             clipPath:
               'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
-          className="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          className="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#FFF8EF] to-[#C2BDAB] opacity-20"
         />
       </div>
       <div
