1362abad54004ee7e90391a2804e64cd64554726
minurisiyara <minurisiyara003@gmail.com>
2025-10-05 17:13:30 +0530

    aboutus commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Nav from "./components/nav";
 import Foot from './components/footer';
+import AboutUs from './Pages/AboutUs';
 
 function App() {
   return (
     <div>
       <Nav />   {/* Render Navbar */}
       <Foot/>   {/*Render Footer*/}
-
+      <AboutUs/> {/*Render AboutUs*/}
     </div>
   );
 }
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -7,18 +7,14 @@ const links = [
 const stats = [
   { name: 'Offices worldwide', value: '12' },
   { name: 'Full-time colleagues', value: '300+' },
-  { name: 'Hours per week', value: '40' },
+  { name: 'Hours per day', value: '40' },
   { name: 'Paid time off', value: 'Unlimited' },
 ]
 
 export default function Example() {
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-      <img
-        alt=""
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center"
-      />
+      
       <div
         aria-hidden="true"
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
@@ -45,10 +41,9 @@ export default function Example() {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Work with us</h2>
+          <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">About Us</h2>
           <p className="mt-8 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-            fugiat veniam occaecat fugiat.
+            Creating magical weddings with elegant planning tools, cultural heritage celebration, and AI-powered assistance. Your perfect day starts here.
           </p>
         </div>
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
