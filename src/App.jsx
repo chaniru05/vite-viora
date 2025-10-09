e69edd62d0ef07bcde18aa875e30729dca78a0be
minurisiyara <minurisiyara003@gmail.com>
2025-10-10 00:36:30 +0530

    YTvideo uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from "./components/nav";
-import Foot from './components/footer';
 import AboutUs from './Pages/AboutUs';
+import Testimonials from './Pages/Testimonials';
+import Foot from './components/footer';
+
 
 function App() {
   return (
     <div>
       <Nav />   {/* Render Navbar */}
-      <Foot/>   {/*Render Footer*/}
       <AboutUs/> {/*Render AboutUs*/}
+      <Testimonials/> {/*Render Testimonials*/}
+      <Foot/>   {/*Render Footer*/}
+      
     </div>
   );
 }
