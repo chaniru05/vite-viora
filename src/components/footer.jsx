7937f7d226244a153f52381d7445ec49554feb0f
chaniru05 <chanirusenarathne05@gmail.com>
2025-10-20 22:54:23 +0530

    blue to red

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -119,7 +119,7 @@ const ComponentName = () => {
                         <input type="email" name="email" id="email" placeholder="Enter your email" className="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                     </div>
 
-                    <button type="submit" className="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700 mx-auto">Subscribe</button>
+                    <button type="submit" className="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-red-800 rounded-md hover:bg-red-700 focus:bg-red-700 mx-auto">Subscribe</button>
                 </form>
             </div>
         </div>
