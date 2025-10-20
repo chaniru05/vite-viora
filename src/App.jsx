953cef53783d1084842c7349a0724565d4f71ed9
chaniru05 <chanirusenarathne05@gmail.com>
2025-10-20 22:56:46 +0530

    Revert "Merge branch 'Chan-Dev' of https://github.com/chaniru05/vite-viora into Chan-Dev"
    This reverts commit 96053feba7275281c97caf762a2fa325f4455cf4, reversing
    changes made to 7937f7d226244a153f52381d7445ec49554feb0f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import './Pages/Home'
-import './Pages/Vendors'
-import './Pages/db/ud'
-import './Pages/AboutUs'
 import Nav from "./components/nav";
 import Foot from './components/footer';
 
 function App() {
   return (
     <div>
-      <Router>
-      <Nav />   {/* Navigation bar stays on all pages */}
-      <Foot/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/vendors" element={<Vendors />} />
-      </Routes>
-      <Footer /> {/* Optional footer */}
-    </Router>
+      <Nav />   {/* Render Navbar */}
+      <Foot/>   {/*Render Footer*/}
 
     </div>
   );
