51b35166e423baf0531ad52ec30c5b381090637e
kavithakaWH <kawithakaisipathana@gmail.com>
2025-10-22 09:19:50 +0530

    vendorspreviewupdate

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Nav from "./components/nav";
 import Foot from './components/footer';
+import Vednorspreview from './Pages/vendorspreview';
 
 function App() {
   return (
     <div>
       <Nav />   {/* Render Navbar */}
       <Foot/>   {/*Render Footer*/}
-
+      <Vednorspreview/>   {/*Render vendorspreview page
+*/}
     </div>
   );
 }
