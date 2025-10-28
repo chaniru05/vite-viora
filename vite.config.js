7ce6f8c095ea39daebad728f42e70cdd97cf2f2b
chaniru05 <chanirusenarathne05@gmail.com>
2025-10-28 11:08:15 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/vite-viora/',
+  plugins: [react(), tailwindcss()],
+  // 🚀 Setting base path to the root
+  base: '/',
 })
