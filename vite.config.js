16a8dc5036ff7df6cf4f3425a38b472010e30607
chaniru05 <chanirusenarathne05@gmail.com>
2025-10-28 09:35:30 +0530

    Set base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/vite-viora/tree/main-copy"
+  plugins: [react(), tailwindcss()],
+  // 🚀 Setting base path to the root
+  base: '/',
 })
